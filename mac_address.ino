62f40fa96f26aaff600cc619b4853be074835a67
Sathwik Shetty N <109843688+sathwikshetty0@users.noreply.github.com>
2025-07-10 18:52:35 +0530

    Create mac_address.ino

---
+++ mac_address.ino
@@ -0,0 +1,18 @@
+#include <WiFi.h>
+
+void setup() {
+  // Initialize Serial Monitor
+  Serial.begin(115200);
+
+  // Set the device to Wi-Fi station mode to initialize the hardware
+  WiFi.mode(WIFI_STA);
+  
+  // Print the MAC address to the Serial Monitor
+  Serial.println("Reading MAC address...");
+  Serial.print("ESP32 MAC Address: ");
+  Serial.println(WiFi.macAddress());
+}
+
+void loop() {
+  // The loop is empty because we only need to get the address once.
+}
